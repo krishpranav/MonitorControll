8d181027088a5f3f50ae034a1a472379d69e0d4d
krishpranav <krisna.pranav@gmail.com>
2021-02-04 10:37:35 +0530

    done changeS

--- monitorcontroll.cpp
+++ monitorcontroll.cpp
@@ -1,20 +1,17 @@
-//a simple c++ tool for turn off the monitor
-//tool author: krisna pranav
-// use this tool for fun purpose and legal purpose
-
-
 //imports
 #include <Windows.h>
 #include <stdio.h>
 
-//main function
+using namespace std;
+
 int main()
 {
+
     //turns off the monitor
-    SendMessage(HWND_BROADCAST, WM_SYSCOMMAND, SC_MONITORPOWER, (LPARAM)2);
+    SendMessage(HWND_BROCAST, WM_SYSCOMMAND, SC_MONITORPOWER, (LPARAM)2);
     sleep(8000); //for 8 seconds
-    //turns monitor on after 8 seconds
-    SendMessage(HWND_BROADCAST, WM_SYSCOMMAND, SC_MONITORPOWER, (LPARAM)2);
+    //turns off the monitor after 8 seconds
+    SendMessage(HWND_BROCAST, WM_SYSCOMMAND, SC_MONITORPOWER, (LPARAM)2);
     return 0;
 }
 
